76d06e19b8a319bd64653d9964568106eadba35a
Roberto Ramirez <robert.624@hotmail.com>
2023-04-05 17:02:03 -0500

    profile page base

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,20 +1,21 @@
 import { type GetStaticProps, type NextPage } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
+import Image from "next/image";
 
 // type PageProps = InferGetStaticPropsType<typeof getStaticProps>;
 
 const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
-
-  const { data, isLoading } = api.profile.getUserByUsername.useQuery({ username});
-
+  const { data, isLoading } = api.profile.getUserByUsername.useQuery({
+    username,
+  });
 
   if (isLoading) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
   if (!data) {
-    return <div>404</div>
+    return <div>404</div>;
   }
 
   return (
@@ -23,20 +24,30 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         <title>{data.username}</title>
       </Head>
       <Layout>
-        <div>{data.username}</div>
+        <div className="h-36 border-b border-slate-400 bg-slate-600 relative">
+          <Image
+            width={128}
+            height={128}
+            src={data.profilePicture}
+            alt={`${data.profilePicture ?? ""}'s profile picture`}
+            className="-mb-[64px] ml-4 absolute left-0 bottom-0 rounded-full border-4 border-black"
+          />
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-bold">{`@${data.username ?? ''}`}</div>
+        <div className="border-b border-slate-400 w-full"></div>
       </Layout>
     </>
   );
 };
 
-import { createProxySSGHelpers } from '@trpc/react-query/ssg';
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 import { appRouter } from "~/server/api/root";
 import { prisma } from "~/server/db";
 import superjson from "superjson";
 import Layout from "~/components/Layout";
 
 export const getStaticProps: GetStaticProps = async (context) => {
-
   const ssg = createProxySSGHelpers({
     router: appRouter,
     ctx: { prisma, userId: null },
@@ -49,20 +60,18 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
   const username = slug.replace("@", "");
 
-  await ssg.profile.getUserByUsername.prefetch({ username })
+  await ssg.profile.getUserByUsername.prefetch({ username });
 
   return {
     props: {
       trpcState: ssg.dehydrate(),
-      username
-    }
-  }
+      username,
+    },
+  };
 };
 
 export const getStaticPaths = () => {
-
-
-  return {paths: [], fallback: "blocking"}
-}
+  return { paths: [], fallback: "blocking" };
+};
 
 export default ProfilePage;
